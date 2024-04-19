d10b38c13261ea0f63ec1a9bdc0c56c6424098c3
SleepyGabe <gaboparinas78@gmail.com>
2024-04-19 23:39:56 +0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+const token = process.env.DISCORD_TOKEN;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`)
+});
+
+client.login(token);
